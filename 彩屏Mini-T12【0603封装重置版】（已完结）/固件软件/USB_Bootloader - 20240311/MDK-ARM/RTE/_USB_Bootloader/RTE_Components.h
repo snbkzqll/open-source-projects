5d8249e7c653be59abb80cb46135bd9530aa7358
snbkzqll <lichangjin0211@gmail.com>
2025-11-18 22:26:24 +0800

    第三次

---
+++ 彩屏Mini-T12【0603封装重置版】（已完结）/固件软件/USB_Bootloader - 20240311/MDK-ARM/RTE/_USB_Bootloader/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'USB_Bootloader_20240310' 
+ * Target:  'USB_Bootloader' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "air32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ 彩屏Mini-T12【0603封装重置版】（已完结）/固件软件/USB_Bootloader - 20240311/MDK-ARM/USB_Bootloader/startup_stm32f103xe.d
@@ -0,0 +1,1 @@
+.\usb_bootloader\startup_stm32f103xe.o: startup_stm32f103xe.s
